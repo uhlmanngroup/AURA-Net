0dff31d359c0986a4ea01bb34f70ac5e29fc8e2b
ethancohen123 <55588944+ethancohen123@users.noreply.github.com>
2021-01-21 20:39:09 +0100

    Add files via upload

---
+++ AURA-net.py
@@ -0,0 +1,247 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+
+from __future__ import print_function, division
+import torch.nn as nn
+import torch.utils.data
+import torch
+from torchvision import models
+
+def convrelu(in_channels, out_channels, kernel, padding):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel, padding=padding),
+        nn.ReLU(inplace=True),
+    )
+
+class conv_block(nn.Module):
+    """
+    Convolution Block 
+    """
+    def __init__(self, in_ch, out_ch):
+        super(conv_block, self).__init__()
+        
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_ch, out_ch, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True))
+
+    def forward(self, x):
+
+        x = self.conv(x)
+        return x
+
+
+class up_conv(nn.Module):
+    """
+    Up Convolution Block
+    """
+    def __init__(self, in_ch, out_ch):
+        super(up_conv, self).__init__()
+        self.up = nn.Sequential(
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        x = self.up(x)
+        return x
+
+
+
+class Attention_block(nn.Module):
+    """
+    Attention Block
+    """
+
+    def __init__(self, F_g, F_l, F_int):
+        super(Attention_block, self).__init__()
+
+        self.W_g = nn.Sequential(
+            nn.Conv2d(F_l, F_int, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(F_int)
+        )
+
+        self.W_x = nn.Sequential(
+            nn.Conv2d(F_g, F_int, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(F_int)
+        )
+
+        self.psi = nn.Sequential(
+            nn.Conv2d(F_int, 1, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(1),
+            nn.Sigmoid()
+        )
+
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, g, x):
+        g1 = self.W_g(g)
+        x1 = self.W_x(x)
+        psi = self.relu(g1 + x1)
+        psi = self.psi(psi)
+        out = x * psi
+        return out
+
+
+def convrelu(in_channels, out_channels, kernel, padding):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel, padding=padding),
+        nn.ReLU(inplace=True),
+    )
+
+
+class AURA-net(nn.Module):
+    
+    def __init__(self, img_ch=3, output_ch=1):
+        super(AURA-net, self).__init__()
+
+        n1 = 64
+        filters = [n1, n1 * 2, n1 * 4, n1 * 8, n1 * 16]
+
+        self.base_model = models.resnet18(pretrained=True)
+        
+
+        self.base_layers = list(self.base_model.children())
+        
+        # size=(N, 64, x.H/2, x.W/2)
+        
+
+        self.layer0 = nn.Sequential(*self.base_layers[:3])
+     
+        self.layer1 = nn.Sequential(*self.base_layers[3:5])
+       
+        self.layer2 = self.base_layers[5]  # size=(N, 128, x.H/8, x.W/8)
+       
+        self.layer3 = self.base_layers[6]  # size=(N, 256, x.H/16, x.W/16)
+        
+        self.layer4 = self.base_layers[7]  # size=(N, 512, x.H/32, x.W/32)
+
+        
+      
+        self.fix=nn.Upsample(scale_factor=4)
+        
+        
+        self.Maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.Maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.Maxpool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.Maxpool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.Conv1 = conv_block(img_ch, filters[0])
+        self.Conv2 = conv_block(filters[0], filters[1])
+        self.Conv3 = conv_block(filters[1], filters[2])
+        self.Conv4 = conv_block(filters[2], filters[3])
+        self.Conv5 = conv_block(filters[3], filters[4])
+
+        self.Up5 = up_conv(filters[4], filters[3])
+        self.Att5 = Attention_block(F_g=filters[3], F_l=filters[3], F_int=filters[2])
+        self.Up_conv5 = conv_block(filters[4], filters[3])
+
+        self.Up4 = up_conv(filters[3], filters[2])
+        self.Att4 = Attention_block(F_g=filters[2], F_l=filters[2], F_int=filters[1])
+        self.Up_conv4 = conv_block(filters[3], filters[2])
+
+        self.Up3 = up_conv(filters[2], filters[1])
+        self.Att3 = Attention_block(F_g=filters[1], F_l=filters[1], F_int=filters[0])
+        self.Up_conv3 = conv_block(filters[2], filters[1])
+
+        self.Up2 = up_conv(filters[1], filters[0])
+        self.Att2 = Attention_block(F_g=filters[0], F_l=filters[0], F_int=32)
+        self.Up_conv2 = conv_block(filters[1], filters[0])
+
+        self.Up1 = up_conv(filters[0], filters[0])
+        self.Att1 = Attention_block(F_g=filters[0], F_l=filters[0],  F_int=32)
+        self.Up_conv1 = conv_block(filters[1], filters[0])
+
+        self.Up0 = up_conv(filters[0], filters[0])
+        self.Att0 = Attention_block(F_g=filters[0], F_l=filters[0],  F_int=32)
+        self.Up_conv0 = conv_block(filters[0], filters[0])
+
+        self.conv_original_size0 = convrelu(3, 64, 3, 1)
+        self.conv_original_size1 = convrelu(64, 64, 3, 1)
+        self.conv_original_size2 = convrelu(128, 64, 3, 1)
+
+        self.Conv = nn.Conv2d(filters[0], output_ch, kernel_size=1, stride=1, padding=0)
+        
+        #self.active = torch.nn.Sigmoid()
+        
+
+    def forward(self, x):
+
+        x_original = self.conv_original_size0(x)
+        x_original = self.conv_original_size1(x_original)
+        
+       
+        
+        e0 = self.layer0(x)
+        
+        
+      
+        e1= self.layer1(e0)
+        
+        
+        e2 = self.layer2(e1)
+    
+        
+        
+        e3 = self.layer3(e2)
+        
+        
+        
+        e4 = self.layer4(e3)
+        
+       
+        e5 = self.Maxpool4(e4)
+        e5 = self.Conv5(e5)
+        
+        
+        d5 = self.Up5(e5)
+        x4 = self.Att5(g=d5, x=e4)
+        d5 = torch.cat((x4, d5), dim=1)
+        d5 = self.Up_conv5(d5)
+       
+
+        d4 = self.Up4(d5)
+        x3 = self.Att4(g=d4, x=e3)
+        d4 = torch.cat((x3, d4), dim=1)
+        d4 = self.Up_conv4(d4)
+        
+        
+        d3 = self.Up3(d4)
+        x2 = self.Att3(g=d3, x=e2)
+        d3 = torch.cat((x2, d3), dim=1)
+        d3 = self.Up_conv3(d3)
+        
+        
+        d2 = self.Up2(d3)
+        x1 = self.Att2(g=d2, x=e1)
+        d2 = torch.cat((x1, d2), dim=1)
+        d2 = self.Up_conv2(d2)
+        
+
+        d1 = self.Up1(d2)
+        x0 = self.Att1(g=d1, x=e0)
+        d1 = torch.cat((x0, d1), dim=1)
+        d1 = self.Up_conv1(d1)
+        
+
+        d0 = self.Up0(d1)
+        x=torch.cat((x_original,d0), dim=1)
+        d0=self.conv_original_size2(x)
+        
+
+        out = self.Conv(d0)
+
+
+
+      #  out = self.active(out)
+
+        return out
